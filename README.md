100bc144b0849093b027989c4cdb2e929162467b
abrahamcruz <abraham.cruz@dimexa.com.pe>
2025-12-24 14:36:28 -0500

    Version final de la tarjeta

---
+++ README.md
@@ -0,0 +1,2 @@
+# Proyecto Dia De La Madre
+Este proyecto está hecho con la finalidad de poder darle un regalo a mi mamá implementando los conocimientos obtenidos hasta la fecha, porque amo a mi madre
---
+++ js/consumo.js
@@ -0,0 +1,27 @@
+function buscar() {
+    const inputBuscar = document.getElementById("inputbuscar");
+    const valorBuscar = inputBuscar.value.toLowerCase();
+
+    fetch('../file/mensajes.json')
+    .then(response => { 
+        if (!response.ok) { 
+            throw new Error('Error en la respuesta: ' + response.status); 
+        } return response.json(); 
+    }).then(data => { 
+        const hoja = document.getElementById("hoja");
+        hoja.innerHTML = obtenerDescripcionPorNombre(data.usuarios, valorBuscar);
+    }).catch(error => console.error('Error cargando JSON:', error));
+}
+
+function obtenerDescripcionPorNombre(lista, nombreBuscado) { 
+    // Normalizamos para evitar problemas de mayúsculas/minúsculas 
+    const usuario = lista.find(u => u.nombre.toLowerCase() === nombreBuscado.toLowerCase()); 
+    return usuario ? usuario.mensaje : "Usuario no encontrado 🚫"; 
+}
+
+function limpiar(){
+    const hoja = document.getElementById("hoja");
+    const inputbuscar = document.getElementById("inputbuscar");
+    inputbuscar.value = '';
+    hoja.innerHTML = '';
+}
